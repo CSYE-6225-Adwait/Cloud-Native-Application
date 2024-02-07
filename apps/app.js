0b55ee8f5af41bc45e655779a6a53cd744995960
Adwait Changan <changan.a@northeastern.edu>
2024-02-07 18:25:49 -0500

    Added DB no exist check

--- apps/app.js
+++ apps/app.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import registerRouter from './routes/index.js';
-import {sequelize} from './db.js';
+import {sequelize,initializeDatabase} from './db.js';
 
-export const initialize = (app) => {
+export const initialize = async (app) => {
     app.use(express.json());
     //dotenv.config();
-    sequelize.sync({alter : true});
+    await initializeDatabase();
+    await sequelize.sync({alter : true});
     registerRouter(app);
 }
 
--- apps/db.js
+++ apps/db.js
@@ -1,9 +1,41 @@
 import dotenv from "dotenv";
 import Sequelize from 'sequelize';
+import mysql2 from 'mysql2/promise';
 
 dotenv.config();
 export const sequelize = new Sequelize(process.env.DATABASENAME, process.env.DATABASEUSERNAME, process.env.DATABASEPASSWORD, {
     host: 'localhost',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    dialectOptions: {
+        useUTC: false, 
+        dateStrings: true,
+        typeCast: true
+    },
+    timezone: '-05:00'
 });
 
+const createDatabase = async () => {
+    const connection = await mysql2.createConnection({
+        host: 'localhost',
+        user: process.env.DATABASEUSERNAME,
+        password: process.env.DATABASEPASSWORD,
+    });
+
+    try {
+        await connection.query(`CREATE DATABASE IF NOT EXISTS ${process.env.DATABASENAME}`);
+        console.log(`Database ${process.env.DATABASENAME} created`);
+    } catch (error) {
+        console.error('Error creating the database');
+    } finally {
+        await connection.end();
+    }
+};
+
+export const initializeDatabase = async () => {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection to the database has been established successfully.');
+    } catch (error) {
+        await createDatabase();
+    }
+};
