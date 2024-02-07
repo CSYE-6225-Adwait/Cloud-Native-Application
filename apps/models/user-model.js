d313482415c2dd2e6729d004bac52bfcf62d452f
Adwait Changan <changan.a@northeastern.edu>
2024-02-07 16:23:49 -0500

    added User model

---
+++ apps/models/user-model.js
@@ -0,0 +1,47 @@
+import {sequelize} from '../db.js';
+import { DataTypes,Sequelize } from 'sequelize';
+
+export const User = sequelize.define('User',{
+    id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+        allowNull: false,
+    },
+    first_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    last_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+            isEmail: true
+        }
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    account_created: {
+        type: DataTypes.DATE,
+        allowNull: false,
+    },
+    account_updated: {
+        type: DataTypes.DATE,
+        allowNull: false,
+    },
+      
+},
+{
+    createdAt: 'account_created',
+    updatedAt: 'account_updated'
+  }
+);
+
+
