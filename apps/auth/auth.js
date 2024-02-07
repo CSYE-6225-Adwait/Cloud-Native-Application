d1a61b19f932403b97c70061cb49aa9383d498c3
Adwait Changan <changan.a@northeastern.edu>
2024-02-07 16:17:31 -0500

    Added auth middleware for get and put request for users

---
+++ apps/auth/auth.js
@@ -0,0 +1,35 @@
+import {User} from "../models/user-model.js";
+import bcrypt from 'bcrypt';
+import { setResponseHeaders } from "../utils.js";
+export const auth = async (request, response, next) => {
+    const authHeader = request.headers.authorization;
+    if(!authHeader){
+        setResponseHeaders(response); 
+        return response.status(401).send(); 
+    }
+    if(!authHeader.split(' ')[0] === 'Basic'){
+        setResponseHeaders(response); 
+        return response.status(401).send();
+    }
+
+    const encodedCredentials = authHeader.split(' ')[1];
+    const credentials = Buffer.from(encodedCredentials, 'base64').toString('utf-8');
+    const [username, password] = credentials.split(':');
+
+    const userFound = await User.findOne({ 
+        where: { username: username } 
+    });
+
+    if(!userFound){
+        setResponseHeaders(response); 
+        return response.status(401).send({message : 'Invalid username or password'});
+    }
+    const checkPassword = bcrypt.compareSync(password, userFound.password);
+    if(!checkPassword){
+        setResponseHeaders(response); 
+        return response.status(401).send({message : 'Invalid username or password'});
+    }
+    next();
+}
+
+//"xyz abc" xyz:abc  Basic lrnwerewr;kewrkwerw
